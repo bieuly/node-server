8049d24830a010da4eafd09dacbb54ba003939cb
Billy Hoang <billy-hoang@hotmail.com>
2018-09-01 03:37:51 -0700

    feat: add endpoint to return all pokemon

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import pokemon from 'pokemongo-json-pokedex/output/pokemon.json'
 
 const port = 3000
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("hello world")
+})
+
+app.get("/pokemon", (req, res) => {
+    res.send(pokemon)
 })
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
